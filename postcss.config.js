e6b032a982ffdbbe78e4a2ca8c3bfe314968a0ac
tmtocb <kittz.esport@gmail.com>
2020-12-27 17:09:34 +0100

    add Tailwind 2.0, navbar, container

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -10,3 +11,4 @@ module.exports = {
     })
   ]
 }
+
