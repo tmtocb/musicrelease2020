c938098564ecbbf87e2871228b5eaf182d45b723
tmtocb <kittz.esport@gmail.com>
2020-12-27 15:11:34 +0100

    add Genre scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_27_140727) do
+ActiveRecord::Schema.define(version: 2020_12_27_141055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "releases", force: :cascade do |t|
     t.string "title"
