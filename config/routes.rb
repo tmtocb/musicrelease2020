23f5aed6ea92979943c750c75a7a828cdcca3dc8
tmtocb <kittz.esport@gmail.com>
2020-12-27 16:30:36 +0100

    display Genre select from collection on root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'releases#index'
+  post 'releases/search'
   resources :genres
   resources :releases
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
